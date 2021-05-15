aef0f75ec50b8f6064ffa90edcfd8d8577a7fd84
marino.sanlorenzo <marino.sanlorenzo@aion.eu>
2021-05-15 15:42:22 +0200

    1st_test

---
+++ src/frontend/callbacks.py
@@ -0,0 +1,14 @@
+import datetime
+
+__all__ = ['get_list_stocks' , 'get_start_date', 'get_end_date']
+
+
+def get_list_stocks() -> list:
+    pass
+
+
+def get_start_date() -> datetime.datetime:
+    pass
+
+def get_end_date() -> datetime.datetime:
+    pass
---
+++ src/utils.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from pandas_datareader import data
+
+
+def get_data(params:dict) -> pd.DataFrame:
+    pass
