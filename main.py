23871e6e954b0c0e90b70c1d28728e1042c5a64a
marino.sanlorenzo <marino.sanlorenzo@aion.eu>
2021-05-18 01:17:24 +0200

    layout_adam_integration

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
 
+from dash_main import app
 from src.frontend.callbacks import *
 from src.constants import params
 from src.utils import *
+from src.frontend.layout import layout
+
 
 ### inputs
 # list of stocks
@@ -13,16 +16,28 @@ from src.utils import *
 
 
 def main():
-    pass
+    ###########################################################
+    #################       BACKEND                  #################
+    ###########################################################
+    params["stocks_info"] = params.get("STOCKS_INFO")
+    params["START_DATE"] = get_start_date()
+    params["END_DATE"] = get_end_date()
+    params["STOCKS_LIST"] = get_list_stocks()
+
+    data_step0 = get_data(params)
+    data_step1 = process_data(data_step0, params)
+
+    ###########################################################
+    #################         FRONTEND                #################
+    ###########################################################
+
+    app.layout = layout
+    app.run_server(debug=True)
 
 
-params["stocks_info"] = params.get("STOCKS_INFO")
-params["START_DATE"] = get_start_date()
-params["END_DATE"] = get_end_date()
-params["STOCKS_LIST"] = get_list_stocks()
 
-data_step0 = get_data(params)
-data_step1 = process_data(data_step0, params)
+
+
 
 
 if __name__ == "__main__":
--- src/frontend/callbacks.py
+++ src/frontend/callbacks.py
@@ -1,8 +1,30 @@
 import datetime
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
 from src.constants import *
+from dash_main import app
 
 __all__ = ["get_list_stocks", "get_start_date", "get_end_date"]
 
+
+@app.callback(Output('container', 'children'), [Input('chosen-stocks', 'value')])
+def display_graphs(value):
+    graphs = []
+    for i in value:
+        graphs.append(dcc.Graph(
+            id='graph-{}'.format(i),
+            figure={
+                'data': [{
+                    'x': [1, 2, 3],
+                    'y': [3, 1, 2]
+                }],
+                'layout': {
+                    'title': 'Graph {}'.format(i)
+                }
+            }
+        ))
+    return html.Div(graphs)
 
 def get_list_stocks() -> list:
     pass
